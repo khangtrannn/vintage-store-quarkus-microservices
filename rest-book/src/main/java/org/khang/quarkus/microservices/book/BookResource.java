ce5efe87c904075c56d9b4ba287311863bb8c7bf
Khang Tran <khangtrann8198@gmail.com>
2023-11-27 23:39:01 +0700

    Implement fault tolerance

--- rest-book/src/main/java/org/khang/quarkus/microservices/book/BookResource.java
+++ rest-book/src/main/java/org/khang/quarkus/microservices/book/BookResource.java
@@ -1,13 +1,18 @@
 package org.khang.quarkus.microservices.book;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.time.Instant;
 
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 
 import jakarta.inject.Inject;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
@@ -30,6 +35,8 @@ public class BookResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Operation(summary = "Creates a new book")
+    @Retry(maxRetries = 1, delay = 2000)
+    @Fallback(fallbackMethod = "fallbackOnCreatingABook")
     public Response createABook(@FormParam("title") String title, @FormParam("author") String author,
             @FormParam("year") int yearOfPublication, @FormParam("genre") String genre) {
         Book book = new Book();
@@ -41,5 +48,26 @@ public class BookResource {
         book.creationDate = Instant.now();
         logger.info("Book created: " + book);
         return Response.status(201).entity(book).build();
+    }
+
+    public Response fallbackOnCreatingABook(@FormParam("title") String title, @FormParam("author") String author,
+            @FormParam("year") int yearOfPublication, @FormParam("genre") String genre) throws FileNotFoundException {
+        Book book = new Book();
+        book.isbn13 = "Will be set later";
+        book.title = title;
+        book.author = author;
+        book.yearOfPublication = yearOfPublication;
+        book.genre = genre;
+        book.creationDate = Instant.now();
+        saveBookOnDisk(book);
+        logger.warn("Book saved on disk: " + book);
+        return Response.status(206).entity(book).build();
+    }
+
+    private void saveBookOnDisk(Book book) throws FileNotFoundException {
+        String bookJson = JsonbBuilder.create().toJson(book);
+        try (PrintWriter out = new PrintWriter("book-" + Instant.now().toEpochMilli() + ".json")) {
+            out.println(bookJson);
+        }
     }
 }
